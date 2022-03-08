57cdf017a25b6fe9c8960d3ed603a3573a47c67f
Belal <bhomaidan@hotmail.com>
2022-03-08 18:46:16 +0100

    update CMakeLists.txt

--- tutorial_ros/src/laser_graphical_display.cpp
+++ tutorial_ros/src/laser_graphical_display.cpp
@@ -34,55 +34,69 @@
  ***********************************************************************************************************************
  */
 
-#include <tutorial_ros/laser_text_display.h>
-#include <cmath>
+#include <tutorial_ros/laser_graphical_display.h>
+#include "visualization_msgs/Marker.h"
 #include "ros/time.h"
-
+#include <cmath>
 
 namespace robair
 {
 /***********************************************************************************************************************
- * Class definitions: LaserTextDisplay
+ * Class definitions: LaserGraphicalDisplay
  */
 
 /***********************************************************
  * Primary methods
  */
+LaserGraphicalDisplay::LaserGraphicalDisplay() {
 
-LaserTextDisplay::LaserTextDisplay() {
-
-    sub_scan = n.subscribe("scan", 1, &LaserTextDisplay::scanCallback, this);
+    sub_scan = n.subscribe("scan", 1, &LaserGraphicalDisplay::scanCallback, this);
+    // Preparing a topic to publish our results. This will be used by the visualization tool rviz
+    pub_laser_graphical_display_marker = n.advertise<visualization_msgs::Marker>("laser_graphical_display_marker", 1); 
 
     new_laser = false;
 
     //INFINTE LOOP TO COLLECT LASER DATA AND PROCESS THEM
     ros::Rate r(10);// this node will run at 10hz
     while (ros::ok()) {
-        ros::spinOnce();//each callback is called once to collect new data: laser
+        ros::spinOnce();//each callback is called once to collect new data: laser + robot_moving
         update();//processing of data
         r.sleep();//we wait if the processing (ie, callback+update) has taken less than 0.1s (ie, 10 hz)
     }
 
 }
-  
-void LaserTextDisplay::update() {
+
+void LaserGraphicalDisplay::update() {
 
     // we wait for new data of the laser
     if ( new_laser )
     {
 
+        new_laser = false;
         ROS_INFO("New data of laser received");
 
+        nb_pts = 0;
         for ( int loop=0 ; loop < nb_beams; loop++ )
+        {
             ROS_INFO("r[%i] = %f, theta[%i] (in degrees) = %f, x[%i] = %f, y[%i] = %f", loop, r[loop], loop, theta[loop]*180/M_PI, loop, current_scan[loop].x, loop, current_scan[loop].y);
 
-        new_laser = false;
+            display[nb_pts] = current_scan[loop];
+
+            colors[nb_pts].r = 0;
+            colors[nb_pts].g = 0;
+            colors[nb_pts].b = 1;
+            colors[nb_pts].a = 1.0;
+
+            nb_pts++;
+        }
+
+        populateMarkerTopic();
 
     }
 
 }
 
-void LaserTextDisplay::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
+void LaserGraphicalDisplay::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
 
     new_laser = true;
     // store the important data related to laserscanner
@@ -107,6 +121,102 @@ void LaserTextDisplay::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan
         current_scan[loop].y = r[loop] * sin(beam_angle);
         current_scan[loop].z = 0.0;
     }
+
+}
+
+void LaserGraphicalDisplay::populateMarkerReference() {
+
+    visualization_msgs::Marker references;
+
+    references.header.frame_id = "laser";
+    references.header.stamp = ros::Time::now();
+    references.ns = "laser_graphical_display";
+    references.id = 1;
+    references.type = visualization_msgs::Marker::LINE_STRIP;
+    references.action = visualization_msgs::Marker::ADD;
+    references.pose.orientation.w = 1;
+
+    references.scale.x = 0.02;
+
+    references.color.r = 1.0f;
+    references.color.g = 1.0f;
+    references.color.b = 1.0f;
+    references.color.a = 1.0;
+    geometry_msgs::Point v;
+
+    v.x =  0.02 * cos(-2.356194);
+    v.y =  0.02 * sin(-2.356194);
+    v.z = 0.0;
+    references.points.push_back(v);
+
+    v.x =  5.6 * cos(-2.356194);
+    v.y =  5.6 * sin(-2.356194);
+    v.z = 0.0;
+    references.points.push_back(v);
+
+    float beam_angle = -2.356194 + 0.006136;
+    // first and last beam are already included
+    for (int i=0 ; i< 723; i++, beam_angle += 0.006136){
+        v.x =  5.6 * cos(beam_angle);
+        v.y =  5.6 * sin(beam_angle);
+        v.z = 0.0;
+        references.points.push_back(v);
+    }
+
+    v.x =  5.6 * cos(2.092350);
+    v.y =  5.6 * sin(2.092350);
+    v.z = 0.0;
+    references.points.push_back(v);
+
+    v.x =  0.02 * cos(2.092350);
+    v.y =  0.02 * sin(2.092350);
+    v.z = 0.0;
+    references.points.push_back(v);
+
+    pub_laser_graphical_display_marker.publish(references);
+
+}
+
+void LaserGraphicalDisplay::populateMarkerTopic(){
+
+    visualization_msgs::Marker marker;
+
+    marker.header.frame_id = "laser";
+    marker.header.stamp = ros::Time::now();
+    marker.ns = "laser_graphical_display";
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::POINTS;
+    marker.action = visualization_msgs::Marker::ADD;
+
+    marker.pose.orientation.w = 1;
+
+    marker.scale.x = 0.05;
+    marker.scale.y = 0.05;
+
+    marker.color.a = 1.0;
+
+    //ROS_INFO("%i points to display", nb_pts);
+    //for ( int time=0; time<5; time ++)
+    for (int loop = 0; loop < nb_pts; loop++) {
+            geometry_msgs::Point p;
+            std_msgs::ColorRGBA c;
+
+            p.x = display[loop].x;
+            p.y = display[loop].y;
+            p.z = display[loop].z;
+
+            c.r = colors[loop].r;
+            c.g = colors[loop].g;
+            c.b = colors[loop].b;
+            c.a = colors[loop].a;
+
+            //ROS_INFO("(%f, %f, %f) with rgba (%f, %f, %f, %f)", p.x, p.y, p.z, c.r, c.g, c.b, c.a);
+            marker.points.push_back(p);
+            marker.colors.push_back(c);
+        }
+
+    pub_laser_graphical_display_marker.publish(marker);
+    populateMarkerReference();
 
 }
 
